0a955f3540bdfd7945fc8e1312bc29eddb327133
Sreehari B S <sreeharibs95@gmail.com>
2017-04-14 16:02:54 +0530

    DDA Line Drawing Program - simplified

--- dda.c
+++ dda.c
@@ -5,75 +5,69 @@
 
 #define ROUND(x) ((int)(x+0.5))
 int xa,xb,ya,yb;
-void display (void)
-{
 
- //DDA Algorithm starts here
- int dx=xb-xa,dy=yb-ya,steps,k;
- float xIncrement,yIncrement,x=xa,y=ya;
- glClear (GL_COLOR_BUFFER_BIT);
- glColor3f (1.0, 0.0, 0.0);
- if(abs(dx)>abs(dy))
- steps=abs(dx);
- else steps=abs(dy);
- xIncrement=dx/(float)steps;
- yIncrement=dy/(float)steps;
 
- glBegin(GL_POINTS);
-	 //Plotting X axis and Y axis
-	 glColor3f (1.0, 1.0, 1.0);
-	 for(int i=-100 ; i<=100 ; i++){
-			glVertex2s(i,0);
-			glVertex2s(0,i);
-	 	}
+ void init(void){
 
-	 //Mark X - optional
-	 for(int i=-2; i<=2 ; i++){
-			 glVertex2s(95+i,4+i);
-			 glVertex2s(95-i,4+i);
-	 	}
+    glClearColor (0.0, 0.0, 0.0, 1.0);
+    glOrtho(0.0, 100.0, 0.0, 100.0, -1.0, 1.0);
 
-	 //Mark Y - optional
-	 for(int i=0; i<=2 ; i++){
-			 glVertex2s(4+i,95+i);
-			 glVertex2s(4-i,95+i);
-			 glVertex2s(4,95-i);
-	 	}
- glEnd();
+ }
 
- glColor3f (1.0, 0.0, 0.0);
- glBegin(GL_LINE_STRIP);
+ void display (void){
 
-	 glVertex2s(ROUND(x),ROUND(y));
-	for(k=0;k<steps;k++){
-		 	x+=xIncrement;
-		 	y+=yIncrement;
+    //DDA Algorithm starts here
+
+    int dx = xb-xa,dy = yb-ya,steps,k;
+    float xIncrement,yIncrement, x = xa, y = ya;
+
+    if(abs(dx)>abs(dy))
+                steps = abs(dx);
+    else        steps = abs(dy);
+
+    xIncrement = dx/(float)steps;
+    yIncrement = dy/(float)steps;
+
+
+    glClear (GL_COLOR_BUFFER_BIT);
+    glColor3f (1.0, 1.0, 1.0);
+
+    glBegin(GL_POINTS);
+
+        glVertex2s(ROUND(x),ROUND(y));
+
+        for(k=0;k<steps;k++){
+
+                x = x+xIncrement;
+                y = y+yIncrement;
 		 	glVertex2s(ROUND(x),ROUND(y));
-		 	printf("%lf %lf\n",x,y);
+
 	 	}
 
 
- glEnd();
- glFlush();
+    glEnd();
+
+    glutSwapBuffers();
  }
 
- void init(void)
- {
- glClearColor (0.0, 0.0, 0.0, 0.0);
- glOrtho(-100.0, 100.0, -100.0, 100.0, -1.0, 1.0);
- }
 
-int main(int argc, char** argv)
-{
- printf("Enter the points\n");
- scanf("%d %d %d %d",&xa,&ya,&xb,&yb);
- glutInit(&argc, argv);
- glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
- glutInitWindowSize (500, 500);
- glutInitWindowPosition (100, 100);
- glutCreateWindow ("Simple DDA ");
- init ();
- glutDisplayFunc(display);
- glutMainLoop();
- return 0;
+int main(int argc, char** argv){
+
+
+    printf("Enter the points\n");
+    scanf("%d %d %d %d",&xa,&ya,&xb,&yb);
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize (500, 500);
+    glutInitWindowPosition (100, 100);
+    glutCreateWindow ("Simple DDA ");
+    init ();
+
+    glutDisplayFunc(display);
+    glutMainLoop();
+
+    return 0;
+
 }
+
