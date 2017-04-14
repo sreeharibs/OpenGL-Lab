b5ebd0ac6930508919294360bf1bb3e981a50b3b
Sreehari B S <sreeharibs95@gmail.com>
2017-04-14 16:51:12 +0530

    Bresenhams Line Drawing - simplified

--- bres.c
+++ bres.c
@@ -3,83 +3,71 @@
 #include<stdlib.h>
 #include<stdio.h>
 
-#define ROUND(x) ((int)(x+0.5))
 int xa,xb,ya,yb,p;
-void display (void)
-{
-
- //Bresenham's Algorithm starts here
- int dx=xb-xa,dy=yb-ya;
- int x=xa,y=ya;
- p=(2*dy)-dx;
 
 
- glClear (GL_COLOR_BUFFER_BIT);
+ void init(void){
 
- glBegin(GL_POINTS);
-	 //Plotting X axis and Y axis
-	 glColor3f (1.0, 1.0, 1.0);
-	 for(int i=-100 ; i<=100 ; i++){
-			glVertex2s(i,0);
-			glVertex2s(0,i);
-	 	}
+    glClearColor (0.0, 0.0, 0.0, 1.0);
+    glOrtho(0.0, 100.0, 0.0, 100.0, -1.0, 1.0);
 
-	 //Mark X - optional
-	 for(int i=-2; i<=2 ; i++){
-			 glVertex2s(95+i,4+i);
-			 glVertex2s(95-i,4+i);
-	 	}
+ }
 
-	 //Mark Y - optional
-	 for(int i=0; i<=2 ; i++){
-			 glVertex2s(4+i,95+i);
-			 glVertex2s(4-i,95+i);
-			 glVertex2s(4,95-i);
-	 	}
- glEnd();
+ void display (void){
 
- glColor3f (1.0, 0.0, 0.0);
- glBegin(GL_LINE_STRIP);
+    //Bresenhams Algorithm starts here
 
-	 printf("%d %d\n",x,y);
-	 glVertex2s(x,y);
+    int dx=xb-xa,dy=yb-ya;
+    int x=xa,y=ya;
+
+    p=(2*dy)-dx;
+
+    glClear (GL_COLOR_BUFFER_BIT);
+    glColor3f (1.0, 1.0, 1.0);
+
+    glBegin(GL_POINTS);
+
+    glVertex2s(x,y);
 
 	 for(int k=0;k<dx;k++){
 		 	if(p<0){
-				p=p+2*dy;
-		 		x++;
+                    p=p+2*dy;
+                    x++;
+            }
+
+			else{
+                    p=p+2*dy-2*dx;
+                    x++;
+                    y++;
 			}
-			else {
-				p=p+2*dy-2*dx;
-				x++;
-				y++;
-			}
-		 	printf("%d %d\n",x,y);
+
 			glVertex2s(x,y);
-	 	}
+    }
 
 
- glEnd();
- glFlush();
+    glEnd();
+
+    glutSwapBuffers();
  }
 
- void init(void)
- {
- glClearColor (0.0, 0.0, 0.0, 0.0);
- glOrtho(-100.0, 100.0, -100.0, 100.0, -1.0, 1.0);
- }
 
-int main(int argc, char** argv)
-{
- printf("Enter the points\n");
- scanf("%d %d %d %d",&xa,&ya,&xb,&yb);
- glutInit(&argc, argv);
- glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
- glutInitWindowSize (500, 500);
- glutInitWindowPosition (100, 100);
- glutCreateWindow ("Simple Bresenham's ");
- init ();
- glutDisplayFunc(display);
- glutMainLoop();
- return 0;
+int main(int argc, char** argv){
+
+
+    printf("Enter the points\n");
+    scanf("%d %d %d %d",&xa,&ya,&xb,&yb);
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize (500, 500);
+    glutInitWindowPosition (100, 100);
+    glutCreateWindow ("Simple DDA ");
+    init ();
+
+    glutDisplayFunc(display);
+    glutMainLoop();
+
+    return 0;
+
 }
+
